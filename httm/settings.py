de5714fc5a6ed5f9ab6fc6deb34d7324b9fdb4a3
Ligirk <ann52999@gmail.com>
2021-06-02 19:13:50 +0700

    update

--- httm/settings.py
+++ httm/settings.py
@@ -25,8 +25,6 @@ SECRET_KEY = '=qx++y9x-n5xk9wwj_appzpz&o8-1#!#)1z=fc#%l731gl2(l7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://httm.herokuapp.com/"]
-
 
 # Application definition
 
